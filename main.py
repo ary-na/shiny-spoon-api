1e7a9a33ffa3e7ae6a88ace008ff330ca9615d45
Arian N. Yamchelo <ariannajafi@outlook.com>
2022-10-22 20:36:40 +1100

    Create API endpoints

--- main.py
+++ main.py
@@ -8,11 +8,31 @@ app = FastAPI()
 ss_logins = init_ss_logins('ss-logins', boto3.resource('dynamodb', 'ap-southeast-2'))
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Create new login
+@app.post('/api/add-login')
+async def add_ss_login(user_name: str, email: str, password: str):
+    ss_logins.add_login(user_name, email, password)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# Get login using username and email
+@app.get("/api/{user_name}/{email}")
+async def get_ss_login(user_name: str, email: str):
+    return ss_logins.get_login(user_name, email)
+
+
+# Update login
+@app.put("/api/update-login")
+async def update_ss_login(user_name: str, email: str, password: str):
+    return ss_logins.update_login(user_name, email, password)
+
+
+# Delete login
+@app.delete('api/delete-login')
+async def delete_ss_login(user_name: str, email: str):
+    ss_logins.delete_login(user_name, email)
+
+
+# Get login using user_name
+@app.get("/api/{user_name}")
+async def get_ss_login(user_name: str):
+    return ss_logins.query_login(user_name)
